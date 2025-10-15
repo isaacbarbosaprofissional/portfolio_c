cdf91d4d278fe57f8b5e1c5c44f02cb71d7a09b8
Isaac Barbosa <isaac.barbosa.contato@gmail.com>
2025-10-15 12:31:12 -0300

    mdc-entre-2.c

--- mdc-entre-2.c
+++ mdc-entre-2.c
@@ -1,25 +1,34 @@
-/*MDC de dois numeros*/
-//By Isaac barbosa
+/*******************************************************************************
+*        This project calculates the GCD (Greatest Common Divisor) of two numbers *
+*                      Author: Isaac dos Santos Barbosa                     *
+*                        Date: April 15, 2025                                *
+*******************************************************************************/
+
 #include <stdio.h>
 
 int main()
 {
-	int n1,n2;
-	printf("Digite dois numeros:\n");
-	scanf("%d\n%d",&n1,&n2);
-	while(n1!=n2)
-	{
-		if(n1>n2)
-		{
-			n1=n1-n2;
-		}
-		else
-		{
-			n2=n2-n1;
-		}
+    int n1, n2;
 
-	}
-	printf("O MDC de dois numeros C): %d",n1);
+    // Ask the user for two numbers
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &n1, &n2); // Read the two numbers
 
-	return 0;
+    // Calculate GCD using Euclidean subtraction method
+    while(n1 != n2)
+    {
+        if(n1 > n2)
+        {
+            n1 = n1 - n2;
+        }
+        else
+        {
+            n2 = n2 - n1;
+        }
+    }
+
+    // Print the GCD
+    printf("The GCD of the two numbers is: %d\n", n1);
+
+    return 0; // Return 0 to indicate successful execution
 }
