1cf12c27d40723ee125c20c3ba0f6a930022b488
Isaac Barbosa <isaac.barbosa.contato@gmail.com>
2025-10-15 11:54:31 -0300

    armazenar_charactere.c

--- armazenar_charactere.c
+++ armazenar_charactere.c
@@ -1,12 +1,18 @@
-#include<stdio.h>
+/*******************************************************************************
+*             This code reads a name from the user and displays it             *
+*                      Author: Isaac dos Santos Barbosa                        *
+*                         Date: April 15, 2025                                 *
+*******************************************************************************/
 
+#include <stdio.h>
 
-void main(void)
+int main(void)  // main should return int
 {
-  char nome[100];
-  
-  printf("Digite seu nome: ");
-  scanf("%s",nome);
-  printf("O nome armazenado foi: %s", nome);
-  
+    char nome[100]; // Declare a character array to store the name
+
+    printf("Enter your name: "); // Ask the user to enter their name
+    scanf("%s", nome); // Read the name from input
+    printf("The stored name is: %s", nome); // Print the stored name
+
+    return 0; // Return 0 to indicate successful execution
 }
