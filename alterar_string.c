25f15133d3bfbc7deaf050b5e7c94ab4cfb32928
Isaac Barbosa <isaac.barbosa.contato@gmail.com>
2025-10-15 11:52:12 -0300

    alterar_string.c

--- alterar_string.c
+++ alterar_string.c
@@ -1,11 +1,17 @@
+/*******************************************************************************
+*             This code demonstrates basic string manipulation                 *
+*                      Author: Isaac dos Santos Barbosa                        *
+*                           Date: April 15, 2025                               *
+*******************************************************************************/
+
 #include <stdio.h>
 
 void main(void)
 {
-    char letra[10] = "Joao";
-    printf("\nA String é: %s", letra);
-    printf("\nA SEGUNDA letra é: %c", letra[1]);
-    letra[1] = 'U';
-    printf("\nAgora a segunda letra é: %c", letra[1]);
-    printf("\n\nA String resultante: %s", letra);
+    char letra[10] = "Joao"; // Declare a string "Joao"
+    printf("\nThe string is: %s", letra); // Print the string
+    printf("\nThe SECOND letter is: %c", letra[1]); // Print the second character of the string
+    letra[1] = 'U'; // Change the second character to 'U'
+    printf("\nNow the second letter is: %c", letra[1]); // Print the updated second character
+    printf("\n\nThe resulting string: %s", letra); // Print the resulting string
 }
